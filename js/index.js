44a9828d8a8fc85d3b2b04e434fd3a9ac49bdfbb
CosmasAlor <alorkuolak22@gmail.com>
2024-07-04 06:26:15 +0300

    core update

--- js/index.js
+++ js/index.js
@@ -1,70 +1,36 @@
 /// <reference types="../@types/jquery"/>
 
-// $('button').on('click', function (e) {
-//     let hide = $('#S-head').next("p").toggleClass('d-none')
-// });
-
-// $(document).ready(function() {
-//     $('.S-head').on('click', function(e) {
-
-//       var $nextBody = $(this).next('.S-body');
-      
-//       $('.S-body').not($nextBody).addClass('d-none');
-
-//       $nextBody.toggleClass('d-none');
-//     });
-//   });
 
 $('.btn-close-white').on('click', function () {
-    $('.menu').animate({width: '0%'}, 500)
-    $('ul').animate({width: '0'}, 1000);
-    $('ul').hide(1000);
-
-})
-
+    $('.menu').animate({ width: '0%' }, 700);
+    $('ul').animate({ width: '0' }, 700, function() {
+        $(this).hide();
+    });
+    $('#head').animate({ marginLeft: '0' }, 700, function() {
+        $(this).removeClass('ml-25');
+    });
+    $('#menu-open').animate({ marginLeft: '0' }, 700, function() {
+        $(this).removeClass('ml-25');
+    });
+});
 $('#menu-open').on('click', function () {
-    $('.menu').animate({width: '25%'}, 1000);
-    $('ul').removeClass('d-none');
-    $('#head').animate(1000).addClass('m-25');
-    $('ul').show();
-
-})
-
-// $(document).ready(function() {
-//     $('#menu-open').on('click', function(e) {
-    
-//       $('#head').addClass('m-25');
-//       $('#menu').removeClass('w-0 d-none');
-//       $('#menu').animate({ width: '25%' }, 1000);
-//     });
-// });
-
-// $(document).ready(function() {
-//     $('.close').on('click', function(e) {
-//       $('#menu').addClass('d-none');
-//       $('#head').removeClass('ml-25');
-//     });
-// });
-  
-
-// $(document).ready(function() {
-//     $('#menu-open').on('click', function(e) {
-//       e.preventDefault();
-//       $('#menu').removeClass('d-none').animate({
-//         width: 'toggle'
-//       }, 15000); // Adjust the duration (in milliseconds) for slower appearance
-//     });
-//   });
+    $('.menu').animate({ width: '250px' }, 700);
+    $('#head').animate({ marginLeft: '250px' }, 700, function() {
+        $(this).addClass('ml-25');
+    });
+    $('#menu-open').animate({ marginLeft: '250px' }, 700, function() {
+        $(this).addClass('ml-25');
+    });
+    $('ul').removeClass('d-none').show();
+});
 
 
-  
-  
-$('.S-head').on('click', function(e) {
+$('.S-head').on('click', function() {
     var $nextBody = $(this).next('.S-body');
     $('.S-body').not($nextBody).addClass('d-none');
     $nextBody.slideToggle(300).toggleClass('d-none');
 });
-  
+
 $('#timer').countdown('2024/8/1', function(e) {
     $('.col-lg-3:eq(0) h3').html(e.strftime('%D Days'));
     $('.col-lg-3:eq(1) h3').html(e.strftime('%H H'));
@@ -72,20 +38,21 @@ $('#timer').countdown('2024/8/1', function(e) {
     $('.col-lg-3:eq(3) h3').html(e.strftime('%S S'));
 });
 
-$('#text-area').on('keyup', function (e) {
-    let massage = $('textarea').val();
-    let characterCount = massage.length;
-
+$('#text-area').on('keyup', function() {
+    let message = $(this).val();
+    let characterCount = message.length;
     const fixedLength = 100;
     let result = fixedLength - characterCount;
-
     $('#remainingCharacters').text(result);
 });
 
 
 
+$('a').on('click', function(event) {
 
-  
-
-
+    var targetId = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(targetId).offset().top
+    }, 1000); // Duration of the animation in milliseconds (1000ms = 1 second)
+});
 
